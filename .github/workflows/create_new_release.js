cff40c82090712f834752e4d5c1e747ac9328f7e
Sang <hoangsang.le@manabie.com>
2021-07-27 11:45:06 +0700

    use js file

---
+++ .github/workflows/create_new_release.js
@@ -0,0 +1,43 @@
+const repo = process.env.REPO;
+const currentReleaseBranch = process.env.CURRENT_RELEASE_BRANCH
+
+const arg = process.argv.splice(2);
+
+
+function getListPRNumber() {
+    let listPRNumber = []
+
+    let limit = 100
+    let page = 1
+
+    let url = `/repos/${ repo }/pulls?per_page=${ limit }&page=${ page }&base=${ currentReleaseBranch }`
+    let result = await github.request(url)
+
+    let dataSize = 0
+
+    while (result.data.length != 0) {
+      dataSize = result.data.length
+
+      for (let i=0; i < dataSize; i++) {
+        listPRNumber.push(result.data[i].number)
+      }
+
+      if (dataSize < limit) {
+        break
+      }
+
+      page++
+
+      url = `/repos/${ repo }/pulls?per_page=${ limit }&page=${ page }&base=${ currentReleaseBranch }`
+      result = await github.request(url)
+    }
+
+    return listPRNumber
+}
+
+switch(arg[0]){
+    case "getListPRNumber":
+        getListPRNumber()
+        break;
+    default:
+}
