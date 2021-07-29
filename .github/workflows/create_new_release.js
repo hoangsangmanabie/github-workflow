b9cca737bc272e295be8cdc04e90cc05d345ba73
Sang <hoangsang.le@manabie.com>
2021-07-29 16:26:35 +0700

    update

--- .github/workflows/create_new_release.js
+++ .github/workflows/create_new_release.js
@@ -3,51 +3,51 @@ const currentReleaseBranch = process.env.CURRENT_RELEASE_BRANCH
 const newReleaseBranch = process.env.NEW_RELEASE_BRANCH
 
 async function getListPRNumber(github) {
-    let listPRNumber = []
+  let listPRNumber = []
 
-    let limit = 100
-    let page = 1
+  let limit = 100
+  let page = 1
 
-    let url = `/repos/${ repo }/pulls?per_page=${ limit }&page=${ page }&base=${ currentReleaseBranch }`
-    let result = await github.request(url)
+  let url = `/repos/${repo}/pulls?per_page=${limit}&page=${page}&base=${currentReleaseBranch}`
+  let result = await github.request(url)
 
-    let dataSize = 0
+  let dataSize = 0
 
-    while (result.data.length != 0) {
-      dataSize = result.data.length
+  while (result.data.length != 0) {
+    dataSize = result.data.length
 
-      for (let i=0; i < dataSize; i++) {
-        listPRNumber.push(result.data[i].number)
-      }
-
-      if (dataSize < limit) {
-        break
-      }
-
-      page++
-
-      url = `/repos/${ repo }/pulls?per_page=${ limit }&page=${ page }&base=${ currentReleaseBranch }`
-      result = await github.request(url)
+    for (let i = 0; i < dataSize; i++) {
+      listPRNumber.push(result.data[i].number)
     }
 
-    return listPRNumber
+    if (dataSize < limit) {
+      break
+    }
+
+    page++
+
+    url = `/repos/${repo}/pulls?per_page=${limit}&page=${page}&base=${currentReleaseBranch}`
+    result = await github.request(url)
+  }
+
+  return listPRNumber
 }
 
 async function requestChange(github, listPRNumber) {
-    let requests = []
-    const totalPR = listPRNumber.length
-    let url = ""
-    for (let i=0;i<totalPR;i++) {
-      url = `POST /repos/${ repo }/pulls/${ listPRNumber[i] }/reviews`
-      requests.push(github.request(url, {
-        event: "REQUEST_CHANGES",
-        body: `Your current release branch is outdated. You must update your PR's base branch to the new release branch ${ newReleaseBranch }`
-      }))
-    }
-    await Promise.all(requests)
+  let requests = []
+  const totalPR = listPRNumber.length
+  let url = ""
+  for (let i = 0; i < totalPR; i++) {
+    url = `POST /repos/${repo}/pulls/${listPRNumber[i]}/reviews`
+    requests.push(github.request(url, {
+      event: "REQUEST_CHANGES",
+      body: `Your current release branch is outdated. You must update your PR's base branch to the new release branch "${newReleaseBranch}"`
+    }))
+  }
+  await Promise.all(requests)
 }
 
 module.exports = {
-    getListPRNumber,
-    requestChange
+  getListPRNumber,
+  requestChange
 }
