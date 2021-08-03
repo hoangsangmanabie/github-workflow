96c5522fd9860a301502faf51e6fdeb3207042ee
Sang <hoangsang.le@manabie.com>
2021-08-03 12:58:52 +0700

    format

--- .github/workflows/create_change_log.js
+++ .github/workflows/create_change_log.js
@@ -127,12 +127,14 @@ async function getCommitOnPR({ github, context }, prNumber) {
   const limit = 100
   let page = 1
   let dataSize = 0
+
   let url = `/repos/{owner}/{repo}/pulls/{pull_number}/commits?per_page=${limit}&page=${page}`
   let result = await github.request(url, {
     owner: context.repo.owner,
     repo: context.repo.repo,
     pull_number: prNumber
   })
+
   while (result.data.length != 0) {
     dataSize = result.data.length
     for (let i = 0; i < dataSize; i++) {
@@ -141,10 +143,13 @@ async function getCommitOnPR({ github, context }, prNumber) {
         message: result.data[i].commit.message
       })
     }
+
     if (dataSize < limit) {
       break
     }
+
     page++
+    
     url = `/repos/{owner}/{repo}/pulls/{pull_number}/commits?per_page=${limit}&page=${page}`
     result = await github.request(url, {
       owner: context.repo.owner,
